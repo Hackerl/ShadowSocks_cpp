60c4717dc68dd5a3b4bd67ca30d5c74d18a6faf3
Hackerl <490021209@qq.com>
2018-10-12 17:49:43 +0800

    Init#Start

---
+++ Project/Common/JSONHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patteliu on 2018/10/12.
+//
+
+#include "JSONHelper.h"
---
+++ Project/Common/JSONHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by patteliu on 2018/10/12.
+//
+
+#ifndef SHADOWSOCKSR_CPP_JSONHELPER_H
+#define SHADOWSOCKSR_CPP_JSONHELPER_H
+
+
+class JSONHelper {
+
+};
+
+
+#endif //SHADOWSOCKSR_CPP_JSONHELPER_H
